91b7912cf13ecf358b936f8115e3976892082616
Education <Education@192.168.111.209>
2020-08-26 18:54:24 +0900

    Commit

---
+++ src/main/java/srvmain002/Main001.java
@@ -0,0 +1,14 @@
+package srvmain002;
+
+public class Main001 {
+	public String getColor(int i) {
+    if (i == 1) {
+        return "赤";
+    } else if (i == 2) {
+        return "青";
+
+    } else {
+        return "1or2を入力してください";
+    }
+	}
+}
